6b59bef612c5cb5ac21d90c6c339de9f02eb8cb4
OmerOzdil <omerozdil54@gmail.com>
2020-11-22 22:53:07 +0000

    First Commit

---
+++ src/test/java/jdbctests/Main.java
@@ -0,0 +1,13 @@
+package jdbctests;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String dbUrl = "jdbc:oracle:thin@54.226.186.24:1521:xe";
+        String dbUsername = "hr";
+        String dbPassword = "hr";
+
+
+
+    }
+}
