c6c6988624bba9100a8e121fb4b1d617559f836f
OmerOzdil <omerozdil54@gmail.com>
2020-11-22 23:04:15 +0000

    First Execution

--- src/test/java/jdbctests/Main.java
+++ src/test/java/jdbctests/Main.java
@@ -1,12 +1,33 @@
 package jdbctests;
 
+import java.sql.*;
+
 public class Main {
     
-    public static void main(String[] args) {
-        String dbUrl = "jdbc:oracle:thin@54.226.186.24:1521:xe";
+    public static void main(String[] args) throws SQLException {
+        String dbUrl = "jdbc:oracle:thin:@54.226.186.24:1521:xe";
         String dbUsername = "hr";
         String dbPassword = "hr";
 
+        // create connection
+        Connection connection = DriverManager.getConnection(dbUrl,dbUsername,dbPassword);
+        // create statement object
+        Statement statement = connection.createStatement();
+        //run query and get the result in resultset object.
+        ResultSet resultSet = statement.executeQuery("select* from regions");
+
+        //move pinter to first row
+        resultSet.next();
+
+        System.out.println(resultSet.getString("region_name"));
+
+
+
+
+        // close all connections
+        resultSet.close();
+        statement.close();
+        connection.close();
 
 
     }
